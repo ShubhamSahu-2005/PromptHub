9394f9f6feeb8282adc5fc7e84d687ac9e598a4f
Shubham Sahu <shubhamsahu5122333@gmail.com>
2024-09-14 22:40:15 +0530

    Suspense added

--- app/update-prompt/page.jsx
+++ app/update-prompt/page.jsx
@@ -1,78 +1,13 @@
-'use client';
-import { useEffect, useState } from "react";
-import { useSession } from "next-auth/react"; // used to know which user is logged in
-import { useRouter, useSearchParams } from "next/navigation";
-import Form from "@components/Form";
+import React, { Suspense } from 'react';
+import EditPrompt from '@components/EditPrompt';
 
-const EditPrompt = () => {
-  const router = useRouter();
-  const { data: session } = useSession();
-  const searchParams = useSearchParams();
-  const promptId = searchParams.get('id');
-  
-  const [submitting, setSubmitting] = useState(false); // to know user is submitting the prompt or not
-  const [post, setPost] = useState({
-    prompt: '',
-    tag: '',
-  });
-
-  // Fetch prompt details and set the post state
-  useEffect(() => {
-    const getPromptDetails = async () => {
-      try {
-        const response = await fetch(`/api/prompt/${promptId}`);
-        if (!response.ok) throw new Error('Failed to fetch prompt details');
-        const data = await response.json();
-        setPost({
-          prompt: data.prompt,
-          tag: data.tag,
-        });
-      } catch (error) {
-        console.error('Error fetching prompt details:', error);
-      }
-    };
-
-    if (promptId) getPromptDetails();
-  }, [promptId]);
-
-  // Handle form submission to update the prompt
-  const updatePrompt = async (e) => {
-    e.preventDefault();
-    setSubmitting(true);
-
-    if (!promptId) return alert('Prompt not found');
-
-    try {
-      const response = await fetch(`/api/prompt/${promptId}`, {
-        method: "PATCH",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          prompt: post.prompt,
-          tag: post.tag,
-        }),
-      });
-
-      if (response.ok) {
-        router.push('/');
-      }
-    } catch (error) {
-      console.error('Error updating the prompt:', error);
-    } finally {
-      setSubmitting(false);
-    }
-  };
-
+const ParentComponent = () => {
   return (
-    <Form
-      type="Edit"
-      post={post}
-      setPost={setPost}
-      submitting={submitting}
-      handleSubmit={updatePrompt}
-    />
+    <Suspense fallback={<div>Loading...</div>}>
+      <EditPrompt />
+    </Suspense>
   );
 };
 
-export default EditPrompt;
+export default ParentComponent;
+
